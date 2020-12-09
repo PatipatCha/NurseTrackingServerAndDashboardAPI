989f92d6e4db0b890d693927b2619192564232f0
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-12-09 09:07:24 +0700

    first

---
+++ api/dataFunction.php
@@ -0,0 +1,327 @@
+<?php
+class dataFunction
+{
+    function FilterDistance($id,$distance)
+    {
+        $filename = 'json/itag.json';
+        $content = trim(file_get_contents($filename));
+        $data_json = json_decode($content, true);
+
+        $iTAG_config = $data_json['config'];
+        $iTAG_device = $data_json['device'];
+
+        for($Anum=0;$Anum<count($iTAG_device);$Anum++)
+        {
+            
+            // $iTAGMACADDRESS = $iTAG_device[$Anum]['mac_address'];
+            $iTAGUUID = $iTAG_device[$Anum]['uuid'];
+            if($iTAGUUID==$id)
+            {
+                $iTAGNAME = $iTAG_device[$Anum]['name'];
+                for($Bnum=0;$Bnum<count($iTAG_config);$Bnum++)
+                {
+                    $CONFIGNAME = $iTAG_config[$Bnum]['name'];
+                    if($iTAGNAME==$CONFIGNAME)
+                    {
+                        $GLOBAL_DISTANCE = $iTAG_config[$Bnum]['global_distance'];
+                        if($distance>=$GLOBAL_DISTANCE)
+                        {
+                            $data = 1;
+                        }
+                        else
+                        {
+                            $data = 0;
+                        }                        
+                    }
+
+                }                
+            }
+
+        }
+
+        return $data;
+    }
+
+    function isAndroidbox($id)
+    {
+        $filename = 'json/androidbox.json';
+        $content = trim(file_get_contents($filename));
+        $data_json = json_decode($content, true);
+        $androidbox_data = $data_json['device']; 
+        for($num=0;$num<count($androidbox_data);$num++)
+        {
+            $device_id = $androidbox_data[$num]['device_id'];
+            if($device_id==$id)
+            {
+                $FLAG = 1;
+            }
+        }
+
+        if($FLAG!=1)
+        {
+            $FLAG = 0;
+        }
+
+        return $FLAG;
+    }
+    
+    function CheckVersionAndGetLists($KIND,$FLAG_TYPE)
+    {
+        if($KIND=='androidbox')
+        {
+            $filename = 'json/androidbox.json';
+        }
+        else if($KIND=='itag')
+        {
+            $filename = 'json/itag.json';
+        }
+
+        $content = trim(file_get_contents($filename));
+        $data_json = json_decode($content, true);
+        $androidbox_data = $data_json; 
+
+        if($FLAG_TYPE=='checkversion')
+        {
+            $version = $androidbox_data['version'];
+            $data = $version;
+        }
+        else if($FLAG_TYPE=='getlists')
+        {
+            $device = $androidbox_data['device'];
+            for($num=0;$num<count($device);$num++)
+            {
+                if($KIND=='androidbox')
+                {
+                    $data[$num] = array(
+                        'device_id'=> $device[$num]['device_id'],
+                        'title'=> $device[$num]['title']
+                    );
+                }
+                else if($KIND=='itag')
+                {
+                    $data[$num] = array(
+                        'mac_address'=> $device[$num]['mac_address'],
+                        'uuid'=> $device[$num]['uuid']
+                        // ,'title'=> $device[$num]['title']
+                    );
+                }
+            }
+        }
+        return $data;
+    }
+
+    static function WriteAndroidboxLOG($data_json)
+    {
+        $androidbox_data = $data_json['androidbox'];
+        // $datelogs = date("Y-m-d");
+        // $filename = "androidboxlogs/".$androidbox_data['device_id']."_".$datelogs.".json";
+        // $filename = "androidboxlogs/".$androidbox_data['device_id'].".json";
+        $filename = $androidbox_data['device_id'].".json";
+        $file_encode = json_encode($data_json,true);
+        file_put_contents($filename, $file_encode );
+        chmod($filename,0777);
+
+        return $file_encode;
+    }
+
+    /*
+    function GetVersionAndroidbox()
+    {
+        $content = trim(file_get_contents("androidbox.json"));
+        $data_json = json_decode($content, true);
+        $androidbox_data = $data_json; 
+        $version = $androidbox_data['version'];
+        return $version;
+    }
+
+    function GetListAndroidbox()
+    {
+        $content = trim(file_get_contents("androidbox.json"));
+        $data_json = json_decode($content, true);
+        $androidbox_data = $data_json; 
+        $device = $androidbox_data['device'];
+
+        for($num=0;$num<count($device);$num++)
+        {
+            $data[$num] = array(
+                'device_id'=> $device[$num]['device_id'],
+                'title'=> $device[$num]['title']
+            );
+        }
+        return $data;
+    }
+
+    function GetVersioniTAG()
+    {
+        $content = trim(file_get_contents("itag.json"));
+        $data_json = json_decode($content, true);
+        $itag_data = $data_json; 
+        $version = $itag_data['version'];
+        return $version;
+    }
+
+    function GetListiTAG()
+    {
+        $content = trim(file_get_contents("itag.json"));
+        $data_json = json_decode($content, true);
+        $itag_data = $data_json; 
+        $device = $itag_data['device'];
+
+        for($num=0;$num<count($device);$num++)
+        {
+            $data[$num] = array(
+                'mac_address'=> $device[$num]['mac_address'],
+                'title'=> $device[$num]['title']
+            );
+        }
+        return $data;
+    }
+    */
+
+    /*
+    function WriteGetDataJSONold()
+    {
+        // if( isset($FLAG_BACKEND) )
+        // {
+        //     if( $FLAG_BACKEND==1 )
+        //     {
+        //         //Backend
+        //         $filename = "json/".$information_data['deviceId'].".json";
+        //         $file_encode = json_encode($data_input,true);
+        //         file_put_contents($filename, $file_encode );
+        //         chmod($filename,0777); 
+                    
+        //             //GET Room
+        //             if( !isset($FLAG_GetDataAPI) )
+        //             {
+        //                 $device_json = trim(file_get_contents("device.json"));
+        //                 $device_json = json_decode($device_json, true);
+            
+        //                 for($getRoom=0;$getRoom<count($device_json['device']);$getRoom++)
+        //                 {
+        //                     $get_nurse_list=[];
+        //                     $device_deviceId = $device_json['device'][$getRoom]['deviceId'];
+        //                     $device_title = $device_json['device'][$getRoom]['title'];
+        //                     $device_ordinal = $device_json['device'][$getRoom]['ordinal'];
+        //                     $device_deviceId_URL = "json/".$device_deviceId.".json";
+            
+        //                     if( file_exists($device_deviceId_URL) )
+        //                     {
+        //                         $iTAG_json = trim(file_get_contents($device_deviceId_URL));
+        //                         $iTAG_json = json_decode($iTAG_json, true);
+        //                         $get_nurse_list = [];
+                                    
+        //                         for($getNurse=0;$getNurse<count($iTAG_json);$getNurse++)
+        //                         {
+        //                             $mac_address = $iTAG_json[$getNurse]['mac_address'];
+        //                             $distance = $iTAG_json[$getNurse]['distance'];
+        //                             $title = $iTAG_json[$getNurse]['title'];
+            
+        //                             // $distance_cal = ($distance + 49.751)/-1.2824;
+        //                             // $distance_rating_5m = -56.38;
+        //                             // $distance_rating_1m = -51.45;
+            
+        //                             if( strstr( $title,"iTAG") || $title=="Redmi AirDots_L" )
+        //                             {
+        //                                 if( $distance>(-80.0) )
+        //                                 {
+        //                                     $get_nurse_list[$getNurse] = array(
+        //                                         'mac_address'=>$mac_address,
+        //                                         'distance'=>$distance,
+        //                                         'title'=>$title,
+        //                                     );
+        //                                 }
+        //                             }
+        //                         } 
+            
+        //                         //Sort iTAG
+        //                         sort($get_nurse_list);
+        //                         foreach ($get_nurse_list as $key => $val) 
+        //                         {
+        //                             $get_nurse_list[$key] = array(
+        //                                 'mac_address'=>$val['mac_address'],
+        //                                 'distance'=>$val['distance'],
+        //                                 'title'=>$val['title'],
+        //                             );
+        //                         }
+            
+        //                     }
+                                
+        //                     if(!isset($get_nurse_list)){ $get_nurse_list=[]; }
+        //                     $DataRoom[$getRoom] = array(
+        //                         "ordinal"=>$device_ordinal,
+        //                         "deviceId"=>$device_deviceId,
+        //                         "room_title"=>$device_title,
+        //                         "nurse_list"=>$get_nurse_list
+        //                     );
+            
+        //                 }
+            
+        //                     //Sort
+        //                     sort($DataRoom);
+        //                     foreach ($DataRoom as $key => $val) {
+        //                         $DataRoom[$key] = array(
+        //                             "ordinal"=>$val['ordinal'],
+        //                             "deviceId"=>$val['deviceId'],
+        //                             "room_title"=>$val['room_title'],
+        //                             "nurse_list"=>$val['nurse_list']
+        //                         );
+        //                     }
+        //                     //
+        //                     if(count($DataRoom)>1)
+        //                     {
+        //                         $DataRoom = $DataRoom;
+        //                     }
+        //                     else
+        //                     {
+        //                         $DataRoom = [$DataRoom];
+        //                     }
+        //                     // $RefURL = array("https://www.gujarattourism.com/file-manager/ebrochure/thumbs/testing_e_brochure_3.pdf","http://www3.eng.psu.ac.th/pec/6/pec6/paper/CoE/PEC6OR170.pdf","https://forums.estimote.com/t/use-rssi-measure-the-distance/3665/3");
+        //                     $GetDataAPI = [
+        //                         "head"=>array("code"=>200,"message"=>"OK"),
+        //                         "body"=>array("room"=> $DataRoom )
+        //                         // ,"footer"=>array("Ref."=>$RefURL)
+        //                     ]; 
+        //                     $filenameGetDataAPI = "json/GetDataAPI.json";
+        //                     $file_encodeGetDataAPI = json_encode($GetDataAPI,true);
+        //                     file_put_contents($filenameGetDataAPI, $file_encodeGetDataAPI );
+        //                     chmod($filenameGetDataAPI,0777);  
+        //                     //
+        //                     unlink($device_deviceId_URL);
+        //                     //
+        //             }
+        //                 //
+                    
+        //             // echo json_encode($GetDataAPI);
+        //     }
+        // }    
+    }
+
+    function writeJSON($FLAG_WRITEJSON,$itag_data)
+    {
+        if($FLAG_WRITEJSON==1)
+        {
+            if(count($itag_data)>0)
+            {
+
+                //
+                $GetDataAPI = array("Name"=>"ABC");
+                //
+                $filenameGetDataAPI = "json/DataJSON.json";
+                $file_encodeGetDataAPI = json_encode($GetDataAPI,true);
+                file_put_contents($filenameGetDataAPI, $file_encodeGetDataAPI );
+                chmod($filenameGetDataAPI,0777);
+            }
+        }
+
+        if( file_exists($filenameGetDataAPI) )
+        {
+            return 200;
+        }
+        else
+        {
+            return 400;
+        }
+    }
+    */
+}
---
+++ api/detect.php
@@ -0,0 +1,243 @@
+<?php 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+session_start();
+header('Content-Type: application/json');
+
+include "dataFunction.php";
+include "roomFunction.php";
+$dataFunction = new dataFunction;
+$roomFunction = new roomFunction;
+
+$content = trim(file_get_contents("php://input"));
+$data_json = json_decode($content, true);
+
+$FLAG_WRITEJSON = 0;
+$FLAG_VIEW = 0;
+$FLAG_APILOG = 0;
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    if( isset($data_json) )
+    {
+        //Androidbox
+        if( isset($data_json['androidbox']) && isset($data_json['androidbox']['device_id']) )
+        { 
+            $FLAG_PARAMS = 1;
+            $androidbox_data = $data_json['androidbox']; 
+            $deviceId = $androidbox_data['device_id'];
+        }
+        else if( isset($data_json['information']) && isset($data_json['androidbox']['deviceId']) )
+        {
+            $FLAG_PARAMS = 1;
+            $androidbox_data = $data_json['information'];
+            $deviceId = $androidbox_data['deviceId'];
+        }
+        else
+        {
+            $FLAG_PARAMS = 0;
+        }
+
+        //iTAG
+        if(isset($data_json['itag']))
+        { 
+            $FLAG_PARAMS2 = 1;
+            $itag_data = $data_json['itag']; 
+        }
+        else if(isset($data_json['nurse']))
+        {
+            $FLAG_PARAMS2 = 1;
+            $itag_data = $data_json['nurse'];
+        }
+        else
+        {
+            $FLAG_PARAMS2 = 0;
+        }
+
+        //List
+        if(isset($itag_data['itag_list']))
+        { 
+            $FLAG_PARAMS_LIST = 1;
+            $itag_list_data = $itag_data['itag_list']; 
+        }
+        else
+        {
+            $FLAG_PARAMS_LIST = 0;
+        }
+
+        //Version
+        if(isset($itag_data['version']))
+        { 
+            $FLAG_PARAMS_VERSION = 1;
+            $itag_version_data = $itag_data['version']; 
+        }
+        else
+        {
+            $FLAG_PARAMS_VERSION = 0;
+        }
+        
+        if( $FLAG_PARAMS==1 && $FLAG_PARAMS2==1 && $FLAG_PARAMS_VERSION==1 )
+        {
+            $isAndroidbox = $dataFunction->isAndroidbox($deviceId);
+            if($isAndroidbox==1)
+            {
+                $GetiTAGversion = $dataFunction->CheckVersionAndGetLists('itag','checkversion');
+                if( $FLAG_PARAMS_LIST==1 )
+                {
+                    if($GetiTAGversion!=$itag_version_data)
+                    {
+                        $itag_list = $dataFunction->CheckVersionAndGetLists('itag','getlists');
+                        $itag_list = ["itag_list"=>$itag_list];
+                    }
+                    else
+                    {
+                        $itag_list = ["itag_list"=>null];
+                    }
+                    $code = 200;
+                    $message = "Send Success";
+                    $version = $GetiTAGversion;
+                    $data = $itag_list;                
+                }
+                else
+                {
+                    $code = 200;
+                    $message = "Send Success => NO HAVE PARAMS LIST";
+                    $version = $GetiTAGversion;
+                    $data = [];
+                }
+
+                if($FLAG_WRITEJSON==1)
+                {
+                    $WRITEJSON = ["footer"=>$dataFunction->WriteAndroidboxLOG($data_json)];
+                }
+                else
+                {
+                    $WRITEJSON = [];
+                    $dataFunction->WriteAndroidboxLOG($data_json);
+                }
+
+                if($FLAG_VIEW==1)
+                {
+                    $GetRoom = ["footer"=>$roomFunction->GetRoom($FLAG_VIEW)];
+                }
+                else
+                {
+                    $GetRoom = [];
+                }
+
+                $data = $data + $GetRoom + $WRITEJSON;
+
+            }
+            else
+            {
+                $code = 200;
+                $message = "Send Success => NO HAVE ANDROIDBOX";
+                $version = 'xxxx2020xxxxx';
+                $data = [];
+            }
+        }
+        else
+        {
+            $code = 400;
+            $message = "NOT PARAMS $FLAG_PARAMS $FLAG_PARAMS2 $FLAG_PARAMS_VERSION  => KICK KICK!!!";
+            $version = 'xxxx2020xxxxx';
+            $data = [];
+        }
+        //End
+    }
+    else
+    {
+        $code = 500;
+        $message = "BODY RAW JSON => KICK KICK!!!";
+        $version = 'xxxx2020xxxxx';
+        $data = [];
+    }
+}
+else if($_SERVER['REQUEST_METHOD']=='GET')
+{
+    $code = 400;
+    $message = "METHOD WHAT => KICK KICK!!!";
+    $version = 'xxxx2020xxxxx';
+    $data = [];
+}
+
+$data = [
+    "head"=>array("code"=>$code,"message"=>$message,"version"=>$version),
+    "body"=>$data
+];
+
+echo json_encode($data,JSON_PRETTY_PRINT);
+ 
+//
+// 
+//Backend
+// 
+//
+
+//Write Log
+$filename = $deviceId."_data_detect_log.json";
+$file_encode = json_encode($data_json,true);
+file_put_contents($filename, $file_encode );
+chmod($filename,0777);  
+
+
+// function Main()
+// {
+//     // $itag_data = $data_json['itag'];
+//     // $androidbox_data = $data_json['androidbox'];
+//     $write_device_id = $androidbox_data['device_id'];
+//     if($write_device_id!='')
+//     {
+//         //Write Log
+//         $filename = "jsonlogs/".$information_data['device_id']."_data_detect.json";
+//         $file_encode = json_encode($data_json,true);
+//         file_put_contents($filename, $file_encode );
+//         chmod($filename,0777);     
+         
+//         //
+//         // for($num=0;$num<count($nurse_data);$num++)
+//         // {
+//         //     $title = $nurse_data[$num]['title'];
+//         //     if($title=='iTAG            ')
+//         //     {
+//         //         $mac_address = $nurse_data[$num]['mac_address'];
+//         //         $distance = $nurse_data[$num]['distance'];
+//         //         $data_input = [array(  'mac_address'=> $mac_address,'title'=> $title,'distance'=> $distance)];
+//         //     }
+//         // }
+
+//         for($num=0;$num<count($nurse_data);$num++)
+//         {
+//             if( isset($nurse_data[$num]['title']) ){$title = $nurse_data[$num]['title'];}else{$title =null;}
+          
+//             $uuid = $nurse_data[$num]['uuid'];
+//             $mac_address = $nurse_data[$num]['mac_address'];
+//             $distance = $nurse_data[$num]['distance'];
+//             $data_input[$num] = array(   'uuid'=> $uuid,'mac_address'=> $mac_address,'title'=> $title,'distance'=> $distance);
+            
+//         }
+        
+//         $data = [
+//             "head"=>array("code"=>200,"message"=>"OK","version"=>''),
+//             "body"=>array( "iTAG"=> $data_input )
+//         ];
+        
+//     }
+//     else
+//     {
+//         $data = [
+//             "head"=>array("code"=>200,"message"=>"Thank You Pong","version"=>''),
+//             "body"=>[]
+//         ];   
+//     }
+// } 
+
+/*
+
+function Main()
+function Write JSON Logs()
+function Write API Logs()
+function Write Get Data JSON()
+
+*/
